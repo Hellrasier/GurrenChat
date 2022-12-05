d71e33651cc33434ee240848cfb560115b8047a1
Hellrasier <denislolipop48@gmail.com>
2022-12-05 13:36:02 +0200

    Caddy server docker setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+
+  server: {
+    host: true,
+    port: 8000,
+    proxy: {
+      '/api': `http://${process.env.API_HOST || 'localhost'}:${process.env.API_PORT || 3000}/`,
+      '/ws': {
+        target: `ws://${process.env.API_HOST || 'localhost'}:${process.env.API_PORT || 3000}/`,
+        changeOrigin: true,
+        ws: true,
+      },
+    
+    }
+	},
 })
